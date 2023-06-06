2d199558bd9c36ccd9347c881ae0a86aff67ac42
sEGuoer <709074535@qq.com>
2023-06-06 17:17:36 +0800

    最终

--- src/D20230606/duixiang.java
+++ src/D20230606/duixiang.java
@@ -2,4 +2,10 @@ package D20230606;
 
 public class duixiang {
     //An object is a software bundle of related state behavior
+    int size;
+
+    public static void main(String[] args) {
+        int size=0;
+        System.out.println(size);
+    }
 }
