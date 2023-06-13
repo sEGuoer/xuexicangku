98f9d8f05c26f7ac2f74259d598d8791e3ddbdf8
sEGuoer <709074535@qq.com>
2023-06-13 09:17:50 +0800

    实现在随机字符串中统计不同字母的个数

--- src/D20230612/JavaTest.java
+++ src/D20230612/JavaTest.java
@@ -2,8 +2,8 @@ package D20230612;
 
 public class JavaTest {
     public static void main(String[] args) {
-/*        String jdk = "jdk";
-*//*        jdk=jdk.replace('j','J');
+        /*        String jdk = "jdk";
+         *//*        jdk=jdk.replace('j','J');
         jdk=jdk.replace('d','D');
         jdk=jdk.replace('k','K');*//*
         jdk = jdk.toUpperCase();
@@ -13,42 +13,49 @@ public class JavaTest {
 
         String howMuch = "298fjdsklzfASDJKLKASDd23";
         char[] chars = howMuch.toCharArray();
+
+        //howMuch.charAt(1);
+
         int daXie = 0;
         int xiaoXie = 0;
         int shuZi = 0;
-        for (int i = 0;i < chars.length;i++){
-            if (chars[i] >= 'a' && chars[i] <= 'z'){
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'a' && chars[i] <= 'z') {
                 xiaoXie++;
-            }else if (chars[i] >= 'A' && chars[i] <= 'Z'){
+            } else if (chars[i] >= 'A' && chars[i] <= 'Z') {
                 daXie++;
-            }else if (chars[i] >= '0' && chars[i] <= '9'){
+            } else if (chars[i] >= '0' && chars[i] <= '9') {
                 shuZi++;
             }
 
         }
-        //System.out.println("大写字母有" + daXie);
+        System.out.println("大写字母有" + daXie);
         System.out.println("数字有" + shuZi);
-        System.out.println("字母有" + xiaoXie );
+        System.out.println("小写字母有" + xiaoXie);
 
-        for (int i = 97 ; i < 97+26 ; i ++){
-            int num = 0 ;
-            for(int j = 97 ; j < 97 + chars.length ; j ++){
-                int charj = (int)chars[j-97];
-                if (charj == i){
+        for (int i = 97; i < 97 + 26; i++) {
+            int num = 0;
+            for (int j = 97; j < 97 + chars.length; j++) {
+                int charj = (int) chars[j - 97];
+                if (charj == i) {
                     num++;
                 }
             }
-            if (num > 0){System.out.println((char)i+":"+num);}
+            if (num > 0) {
+                System.out.println((char) i + ":" + num);
+            }
         }
-        for (int i = 65 ; i < 65+26 ; i ++){
-            int num = 0 ;
-            for(int j = 65 ; j < 65 + chars.length ; j ++){
-                int charj = (int)chars[j-65];
-                if (charj == i){
+        for (int i = 65; i < 65 + 26; i++) {
+            int num = 0;
+            for (int j = 65; j < 65 + chars.length; j++) {
+                int charj = (int) chars[j - 65];
+                if (charj == i) {
                     num++;
                 }
             }
-            if (num > 0){System.out.println((char)i+":"+num);}
+            if (num > 0) {
+                System.out.println((char) i + ":" + num);
+            }
         }
 //        int j = (int)'a';
 //        System.out.println(j);
