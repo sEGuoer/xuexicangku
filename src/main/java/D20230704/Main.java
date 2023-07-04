65eac266144b917800983eed8fbcf9431daafa50
sEGuoer <709074535@qq.com>
2023-07-04 16:47:29 +0800

    注解使用

---
+++ src/main/java/D20230704/Main.java
@@ -0,0 +1,8 @@
+package D20230704;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.getAge();
+    }
+}
---
+++ src/main/java/D20230704/Person.java
@@ -0,0 +1,14 @@
+package D20230704;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Generated
+public class Person {
+    private String name;
+    private int age;
+    private String content;
+}
