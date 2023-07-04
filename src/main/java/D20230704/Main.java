c01384457fa8e0f071c709324d64c9bc462aa40e
sEGuoer <709074535@qq.com>
2023-07-04 17:03:49 +0800

    让地址相同的一个方法

--- src/main/java/D20230704/Main.java
+++ src/main/java/D20230704/Main.java
@@ -1,8 +1,12 @@
 package D20230704;
 
 public class Main {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.getAge();
+    public static void main (String[] args) {
+        Person person1 = Person.setPerson("1",2,"1");
+        Person person2 = Person.setPerson("1",2,"1");
+        Person person3 = Person.setPerson("1",2,"1");
+        System.out.println(person1 == person2);
+        System.out.println(person3 == person2);
+        System.out.println(person1 == person3);
     }
 }
--- src/main/java/D20230704/Person.java
+++ src/main/java/D20230704/Person.java
@@ -2,13 +2,19 @@ package D20230704;
 
 import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Generated
 public class Person {
+    private static Person person = new Person();
     private String name;
     private int age;
     private String content;
+
+    private Person() {
+    }
+
+    public static Person setPerson(String name, int age, String content) {
+        person.age = age;
+        person.name= name;
+        person.content=content;
+        return person;
+    }
 }
