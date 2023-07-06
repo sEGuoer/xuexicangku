18545c5c5c4761c91288f459f0894e31c1c0f71b
sEGuoer <709074535@qq.com>
2023-07-06 17:10:19 +0800

    jsoup运用

--- src/main/java/D20230706/URLConnectionReader.java
+++ src/main/java/D20230706/URLConnectionReader.java
@@ -1,4 +1,18 @@
 package D20230706;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+
 public class URLConnectionReader {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect( "http://www.baidu.com/").get();
+
+        Element links = doc.selectFirst("a#s-top-loginbtn");
+        System.out.println(links.html());
+    }
 }
--- src/main/java/D20230706/URLReading.java
+++ src/main/java/D20230706/URLReading.java
@@ -1,4 +1,15 @@
 package D20230706;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class URLReading {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://www.douban.com/").get();
+        String title = doc.title();
+        System.out.println(title);
+    }
 }
