5f2743e54b404cbcc5f98b4b5fa1647a1e036cc6
sEGuoer <709074535@qq.com>
2023-07-07 15:40:20 +0800

    首次访问时拿到前四页的内容

--- src/main/java/D20230707/SelectHouseAndThings.java
+++ src/main/java/D20230707/SelectHouseAndThings.java
@@ -5,41 +5,53 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SelectHouseAndThings {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://bbs.xmfish.com/thread-htm-fid-55-page-1.html").get();
+        int page = 1;
+        int i = 0;
         String a = null;
+
+
+        Document doc = Jsoup.connect("http://bbs.xmfish.com/thread-htm-fid-55-page-1.html").get();
+        //拿到需要过滤掉的内容
+//        System.out.println(i);
+        Elements links1 = doc.select("tr.tr3:has(a[class = f14 s4 view])");
+//            #threadlist tr[class="tr3"] td[class="subject"] a 另一种方法
         Elements link = doc.select("tr.tr3 td.subject:has(i[class = windthread_new]) ");
         for (Element e : link) {
             /*System.out.println(e.html());*/
             a = e.html();
         }
-        //拿到需要过滤掉的内容
-//        System.out.println(i);
-        Elements links1 = doc.select("tr.tr3:has(a[class = f14 s4 view])" );
-        int i = 0;
-//            #threadlist tr[class="tr3"] td[class="subject"] a 另一种方法
-        for (Element e : links1) {
-            if (!e.html().equals(a)) {
-                String str = e.text();
-                String[] data = str.split(" ");
-                StringBuilder sb = new StringBuilder();
-                sb.append(data[0]+" ");
-                sb.append(data[1]);
-                System.out.println(sb.toString());
-            } else if (e.html().equals(a)) {
-                i++;
-            }
-        }
-        //拿到想要的信息
-        System.out.println("过滤了" + i + "条");
+
         String firstTime = "";
         Elements links2 = doc.select("tr.tr3:has(a[class = f14 s4 view]) td[class = author]:has(a[title]) p a");
-        for (Element e : links2){
+        for (Element e : links2) {
             System.out.println(e.attr("title"));
             break;
         }
+        //拿到第一个时间
+        File file = new File("");
+
+        for (; page < 4; page++) {
+
+            Document doc1 = Jsoup.connect("http://bbs.xmfish.com/thread-htm-fid-55-page-" + page + ".html").get();
+            for (Element e : links1) {
+                if (!e.html().equals(a)) {
+                    String str = e.text();
+                    String[] data = str.split(" ");
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(data[0]+" ");
+                    sb.append(data[1]);
+                    System.out.println(sb.toString());
+                } else if (e.html().equals(a)) {
+                    i++;
+                }
+            }
+            //拿到想要的信息
+        }
+        System.out.println("过滤了" + i + "条");
     }
 }
